786c1a78b3e5a33ba451b34b72a37799f7a4fa33
Colin Harris <col.w.harris@gmail.com>
2014-12-03 00:08:14 +0800

    Added access modifiers. They're required when using this as a framework.

--- HAL/HAL.swift
+++ HAL/HAL.swift
@@ -9,13 +9,13 @@
 import Foundation
 import Alamofire
 
-class HAL {
-
+public class HAL {
+    
     var _attributes: [String: AnyObject] = [String: AnyObject]()
     var _links: [String: AnyObject] = [String: AnyObject]()
     var _embedded: [String: AnyObject] = [String: AnyObject]()
     
-    init(response: [String: AnyObject]) {
+    internal init(response: [String: AnyObject]) {
         
         if let links = response["_links"] as? [String: AnyObject] {
             self._links = links
@@ -30,11 +30,11 @@ class HAL {
         self._attributes.removeValueForKey("_embedded")
     }
     
-    class func get(url: String) -> Promise<HAL> {
+    public class func get(url: String) -> Promise<HAL> {
         return HAL._loadRequest( Alamofire.request(.GET, url, parameters: nil) )
     }
     
-    class func _loadRequest(request: Alamofire.Request) -> Promise<HAL> {
+    private class func _loadRequest(request: Alamofire.Request) -> Promise<HAL> {
         return Promise<HAL> { (success, failure) in
             request.responseJSON { (request, response, data, error) in
                 if( error != nil ) {
@@ -47,73 +47,73 @@ class HAL {
             return
         }
     }
-
-    func get() -> Promise<HAL> {
+    
+    public func get() -> Promise<HAL> {
         return get("self", params: nil)
     }
     
-    func get(key: String, params: [String: AnyObject]? = nil) -> Promise<HAL> {
+    public func get(key: String, params: [String: AnyObject]? = nil) -> Promise<HAL> {
         var link = self.link(key)
         return HAL._loadRequest( Alamofire.request(.GET, link, parameters: params) )
     }
-
-    func post(params: [String: AnyObject]) -> Promise<HAL> {
+    
+    public func post(params: [String: AnyObject]) -> Promise<HAL> {
         return post("self", params: params)
     }
     
-    func post(key: String, params: [String: AnyObject]) -> Promise<HAL> {
+    public func post(key: String, params: [String: AnyObject]) -> Promise<HAL> {
         var link = self.link(key)
         return HAL._loadRequest( Alamofire.request(.POST, link, parameters: params) )
     }
-
-    func put(params: [String: AnyObject]) -> Promise<HAL> {
+    
+    public func put(params: [String: AnyObject]) -> Promise<HAL> {
         return put("self", params: params)
     }
     
-    func put(key: String, params: [String: AnyObject]) -> Promise<HAL> {
+    public func put(key: String, params: [String: AnyObject]) -> Promise<HAL> {
         var link = self.link(key)
         return HAL._loadRequest( Alamofire.request(.PUT, link, parameters: params) )
     }
-
-    func patch(params: [String: AnyObject]) -> Promise<HAL> {
+    
+    public func patch(params: [String: AnyObject]) -> Promise<HAL> {
         return patch("self", params: params)
     }
     
-    func patch(key: String, params: [String: AnyObject]) -> Promise<HAL> {
+    public func patch(key: String, params: [String: AnyObject]) -> Promise<HAL> {
         var link = self.link(key)
         return HAL._loadRequest( Alamofire.request(.PATCH, link, parameters: params) )
     }
-
-    func delete() -> Promise<HAL> {
+    
+    public func delete() -> Promise<HAL> {
         return delete("self")
     }
     
-    func delete(key: String) -> Promise<HAL> {
+    public func delete(key: String) -> Promise<HAL> {
         var link = self.link(key)
         return HAL._loadRequest( Alamofire.request(.DELETE, link) )
     }
     
-    func attributes() -> [String: AnyObject] {
+    public func attributes() -> [String: AnyObject] {
         return _attributes
     }
     
-    func attribute(key: String) -> AnyObject! {
+    public func attribute(key: String) -> AnyObject! {
         return _attributes[key]
     }
     
-    func links() -> [String: AnyObject] {
+    public func links() -> [String: AnyObject] {
         return _links
     }
     
-    func link(key: String) -> String {
+    public func link(key: String) -> String {
         return _links[key] as String
     }
     
-    func embedded() -> [String: AnyObject] {
+    public func embedded() -> [String: AnyObject] {
         return _embedded
     }
     
-    func embedded(key: String) -> AnyObject? {
+    public func embedded(key: String) -> AnyObject? {
         let value: AnyObject? = _embedded[key]?
         if value != nil {
             if value is Array<[String: AnyObject]> {
